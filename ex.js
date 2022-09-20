b40f1e6ed27b4171a3f32cb1e7fab9bb9cb7e0e1
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-20 20:12:26 -0300

    Criação de alguns objetos e colocados dentro de um array.

--- ex.js
+++ ex.js
@@ -10,3 +10,30 @@ class Carro{
     }
 }
 
+let carros = []
+
+let fer = new Carro;
+fer.equipe = "Ferrari"
+fer.potencia = parseInt(500)
+fer.velMax = parseFloat(360)
+fer.aceleracao = parseFloat(3)
+
+let mer = new Carro;
+mer.equipe = "Mercedes"
+mer.potencia = parseInt(480)
+mer.velMax = parseFloat(350)
+mer.aceleracao = parseFloat(4)
+
+let mc = new Carro;
+mc.equipe = "McLaren"
+mc.potencia = parseFloat(530)
+mc.velMax = parseFloat(370)
+mc.aceleracao = parseFloat(2.5)
+
+let red = new Carro;
+red.equipe = "Red Bull"
+red.potencia = parseFloat(580)
+red.velMax = parseFloat(385)
+red.aceleracao = parseFloat(1.6)
+
+carros.push(fer,mer,mc,red)
