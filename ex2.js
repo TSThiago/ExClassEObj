46e3170f85062df6aef1d7e67ffcb71db707eaa0
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-20 22:16:04 -0300

    Criação de objetos de classe 'Livro' e colocados na array 'arrayLivros'.

--- ex2.js
+++ ex2.js
@@ -1,3 +1,5 @@
+let arrayLivros = []
+
 class Livro{
     Titulo
     Autor
@@ -5,3 +7,26 @@ class Livro{
     AnoDePublicacao
     Disponibilidade
 }
+
+let livroUm = new Livro;
+livroUm.Titulo = "Livro A"
+livroUm.Autor = "Luiz"
+livroUm.Editora = "Editora A"
+livroUm.AnoDePublicacao = 2008
+livroUm.Disponibilidade = true
+
+let livroDois = new Livro;
+livroDois.Titulo = "Livro B"
+livroDois.Autor = "Anderson"
+livroDois.Editora = "Editora B"
+livroDois.AnoDePublicacao = 2012
+livroDois.Disponibilidade = false
+
+let livroTres = new Livro;
+livroTres.Titulo = "Livro C"
+livroTres.Autor = "Shoiti"
+livroTres.Editora = "Editora C"
+livroTres.AnoDePublicacao = 2005
+livroTres.Disponibilidade = false
+
+arrayLivros.push(livroUm,livroDois,livroTres)
