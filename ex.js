ec0e59d0da66ad2f74e81d629140d0e596dc67b3
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-09-20 22:01:52 -0300

    Ajustes de acordo com a correção.

--- ex.js
+++ ex.js
@@ -1,12 +1,12 @@
 // Parte Carro
 class Carro{
-    equipe
-    potencia
-    velMax
-    aceleracao
+    Equipe
+    Potencia
+    VelMax
+    Aceleracao
 
     CalcularTempo(distanciaParametro){
-        let resultado = distanciaParametro / (this.velMax / this.aceleracao)
+        let resultado = distanciaParametro / (this.VelMax / this.Aceleracao)
         return resultado
     }
 }
@@ -14,57 +14,57 @@ class Carro{
 let carros = []
 
 let fer = new Carro;
-fer.equipe = "Ferrari"
-fer.potencia = parseInt(500)
-fer.velMax = parseFloat(360)
-fer.aceleracao = parseFloat(3)
+fer.Equipe = "Ferrari"
+fer.Potencia = parseInt(500)
+fer.VelMax = parseFloat(360)
+fer.Aceleracao = parseFloat(3)
 
 let mer = new Carro;
-mer.equipe = "Mercedes"
-mer.potencia = parseInt(480)
-mer.velMax = parseFloat(350)
-mer.aceleracao = parseFloat(4)
+mer.Equipe = "Mercedes"
+mer.Potencia = parseInt(480)
+mer.VelMax = parseFloat(350)
+mer.Aceleracao = parseFloat(4)
 
 let mc = new Carro;
-mc.equipe = "McLaren"
-mc.potencia = parseFloat(530)
-mc.velMax = parseFloat(370)
-mc.aceleracao = parseFloat(2.5)
+mc.Equipe = "McLaren"
+mc.Potencia = parseFloat(530)
+mc.VelMax = parseFloat(370)
+mc.Aceleracao = parseFloat(2.5)
 
 let red = new Carro;
-red.equipe = "Red Bull"
-red.potencia = parseFloat(580)
-red.velMax = parseFloat(385)
-red.aceleracao = parseFloat(1.6)
+red.Equipe = "Red Bull"
+red.Potencia = parseFloat(580)
+red.VelMax = parseFloat(385)
+red.Aceleracao = parseFloat(1.6)
 
 carros.push(fer,mer,mc,red)
 
 // Parte Corrida
 
 class Corrida{
-    nome
-    tipo
-    distancia
-    vencedor
+    Nome
+    Tipo
+    Distancia
+    Vencedor
 
     DefinirVencedor(){
-        let ganhador = Infinity
+        let menorTempo = Infinity
         carros.forEach((x,index) => {
-            if(x.CalcularTempo(this.distancia) < ganhador){
-                ganhador = x.CalcularTempo(this.distancia)
-                this.vencedor = x.equipe
+            if(x.CalcularTempo(this.Distancia) < menorTempo){
+                menorTempo = x.CalcularTempo(this.Distancia)
+                this.Vencedor = x.Equipe
             }
         })
     }
 
     MostrarVencedor(){
-        alert("Vencedor: " +this.vencedor)
+        alert("Vencedor: " +this.Vencedor)
     }
 }
 
 let inter = new Corrida;
-inter.nome = "Interlagos"
-inter.tipo = "Fórmula 1"
-inter.distancia = 4300
+inter.Nome = "Interlagos"
+inter.Tipo = "Fórmula 1"
+inter.Distancia = 4300
 inter.DefinirVencedor()
 inter.MostrarVencedor()
